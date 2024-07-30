77a2915480a7b2cd5e07219df55c9aa685fd7c81
Linuka Rivi Rihan <linukarivirihan@gmail.com>
2024-07-30 23:19:08 +0530

    packages and APIs implemented

---
+++ src/main/java/com/example/springboot_app/service/UserService.java
@@ -0,0 +1,42 @@
+package com.example.springboot_app.service;
+
+import com.example.springboot_app.dto.UserDTO;
+import com.example.springboot_app.model.User;
+import com.example.springboot_app.repository.UserRepository;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public List<UserDTO> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        return modelMapper.map(users, new TypeToken<List<UserDTO>>(){}.getType());
+    }
+
+    public UserDTO saveUser(UserDTO userDTO) {
+        userRepository.save(modelMapper.map(userDTO, User.class));
+        return userDTO;
+    }
+
+    public UserDTO updateUser(UserDTO userDTO) {
+        userRepository.save(modelMapper.map(userDTO, User.class));
+        return userDTO;
+    }
+
+    public String deleteUser(UserDTO userDTO) {
+        userRepository.delete(modelMapper.map(userDTO, User.class));
+        return "User deleted";
+    }
+}
